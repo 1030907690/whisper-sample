48aec139537df23b6a9f5515f75948728b5945a3
zhouzhongqing <zhouzhongqing@qq.com>
2025-05-04 10:32:35 +0800

    feat:whisper测试

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     模型下载地址 在__init__.py _MODELS
     '''
     model = whisper.load_model("D:/Download/ABDM/large-v3-turbo.pt")
-    result = model.transcribe("D:/Download/ABDM/Music/16k16bit.mp3", fp16=False)
+    result = model.transcribe("16k16bit.mp3", fp16=False)
     print(result["text"])
